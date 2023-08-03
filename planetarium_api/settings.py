abc62747ed467c5754c0eb5be8d347c38ecfaae1
chipa <mezismezis@gmail.com>
2023-08-03 14:17:54 +0300

    update settings
    add rest, drf and media

--- planetarium_api/settings.py
+++ planetarium_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "drf_spectacular",
+    "debug_toolbar",
     "planetarium_service",
 ]
 
@@ -118,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/vol/web/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
